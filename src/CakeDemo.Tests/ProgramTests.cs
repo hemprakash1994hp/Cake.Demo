171beae98f0583e3f6fe40df7c9bf0cba798b377
hemprakash1994hp <44049055+hemprakash1994hp@users.noreply.github.com>
2018-10-25 12:35:39 +0530

    Update ProgramTests.cs

--- src/CakeDemo.Tests/ProgramTests.cs
+++ src/CakeDemo.Tests/ProgramTests.cs
@@ -1,10 +1,6 @@
-#using Ploeh.AutoFixture.Xunit2;
+using Ploeh.AutoFixture.Xunit2;
 using Shouldly;
 using Xunit;
-using Ploeh;
-using System;
-using Theory;
-using AutoData;
 
 
 namespace CakeDemo.Tests
